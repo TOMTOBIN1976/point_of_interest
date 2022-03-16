7ed94ca2b1aab9ee5d3511e23d34ef03be39ad28
Tom Tobin <tomtobin0@gmail.com>
2022-03-16 23:10:15 +0000

    Add .env file & logic for storing cookie details

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import Vision from "@hapi/vision";
 import Hapi from "@hapi/hapi";
 import Cookie from "@hapi/cookie";
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import Handlebars from "handlebars";
@@ -8,8 +9,16 @@ import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
 
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 async function init() {
   const server = Hapi.server({
@@ -34,8 +43,8 @@ async function init() {
 
   server.auth.strategy("session", "cookie", {
     cookie: {
-      name: "poi",
-      password: "secretpasswordnotrevealedtoanyone",
+      name: process.env.COOKIE_NAME,
+      password: process.env.COOKIE_PASSWORD,
       isSecure: false,
     },
     redirectTo: "/",
