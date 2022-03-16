8b98a59ec9f56c8b217c9ae0da775dfdc16c6eb5
Tom Tobin <tomtobin0@gmail.com>
2022-03-16 23:16:58 +0000

    clear cookie

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -40,12 +40,11 @@ export const accountsController = {
     },
   },
   logout: {
-    auth: false,
     handler: function (request, h) {
+      request.cookieAuth.clear();
       return h.redirect("/");
     },
   },
-
   async validate(request, session) {
     const user = await db.userStore.getUserById(session.id);
     if (!user) {
